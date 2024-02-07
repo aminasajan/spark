7aa4a3c50e6980124dd458c241c7638b1becbf56
aminasajan <aminasajan2001@gmail.com>
2024-02-07 15:04:39 +0530

    sample test of Main

---
+++ src/main/scala/com/amina/sampletest.scala
@@ -0,0 +1,30 @@
+package com.amina
+
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object sampletest {
+  def main(args:Array[String]):Unit={
+    val spark=SparkSession.builder()
+      .appName("spark-project1")
+      .master("local[*]")
+      .getOrCreate()
+    val df:DataFrame=spark.read
+      .option("header",value=true)
+      .option("inferSchema",value=true)
+      .csv("data/AAPL.csv")
+ df.show()
+   df.select("Date","Open","Close").show()
+   val column= df("Date")
+//    val columnDF = df.select(column)
+
+    df.select(column).show()
+
+   col("Date")
+ import spark.implicits._
+    $"Date"
+    df.select(col("Date"),$"Open",df("Close")).show()
+
+  }
+
+}
