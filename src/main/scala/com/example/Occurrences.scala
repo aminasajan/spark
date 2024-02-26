611889eab9983ee3573b809863c7b6739f32f697
aminasajan <aminasajan2001@gmail.com>
2024-02-26 11:53:53 +0530

    changes made

--- src/main/scala/com/example/Occurrences.scala
+++ src/main/scala/com/example/Occurrences.scala
@@ -5,32 +5,43 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
 object Occurrences {
-def main(args:Array[String]):Unit= {
-  Logger.getLogger("org").setLevel(Level.ERROR)
-  val spark= SparkSession.builder()
-    .appName("word occurance")
-    .master("local[*]")
-    .getOrCreate()
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = SparkSession.builder()
+      .appName("word occurance")
+      .master("local[*]")
+      .getOrCreate()
 
-  val filepath = "/home/aminasajan/Downloads/ramen-ratings.csv"
+    val filepath = "/home/aminasajan/Downloads/ramen-ratings.csv"
 
-  val csvDF= spark.read.option("header",value = true).csv(filepath)
-  csvDF.printSchema()
-  csvDF.show(truncate=false)
-var status=" "
-do {
-  println(s"Enter the word to be count: ")
+    val csvDF = spark.read.option("header", value = true).csv(filepath)
+    csvDF.printSchema()
+    csvDF.show(truncate = false)
+    //var status=" "
+    //do {
+    //  println(s"Enter the word to be count: ")
+    //
+    //  val word = scala.io.StdIn.readLine()
+    //  val wordCount = countWord(word, csvDF)
+    //  println(s"No. of occurrences of '$word': '$wordCount'")
+    //  println("Do you want to continue(Y/N)")
+    //  status = scala.io.StdIn.readLine()
+    //}while(status == "Y")
 
-  val word = scala.io.StdIn.readLine()
-  val wordCount = countWord(word, csvDF)
-  println(s"No. of occurrences of '$word': '$wordCount'")
-  println("Do you want to continue(Y/N)")
-  status = scala.io.StdIn.readLine()
-}while(status == "Y")
-}
-  def countWord(word:String,csvDF:DataFrame): Long = {
-    val wordCount=csvDF.columns.map(colName=>csvDF.filter(col(colName)===word).count())
-    wordCount.sum
+    val brandWordsDF = csvDF.withColumn("words", split(lower(col("Brand")), "\\s+")).select(explode(col("words")).as("word"))
+    val varietyWordsDF = csvDF.withColumn("words", split(lower(col("Variety")), "\\s+")).select(explode(col("words")).as("word"))
+    val allWordsDF = brandWordsDF.union(varietyWordsDF)
+    val wordCountDF = allWordsDF.groupBy("word").count().orderBy(desc("count"))
+    wordCountDF.show(Int.MaxValue)
+
+
   }
+
+
+  //
+  //  def countWord(word:String,csvDF:DataFrame): Long = {
+  //    val wordCount=csvDF.columns.map(colName=>csvDF.filter(col(colName)===word).count())
+  //    wordCount.sum
+  //  }
 }
 
