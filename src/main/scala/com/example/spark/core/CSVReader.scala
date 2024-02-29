81f728158014e066c997046afaca00af025b07d7
aminasajan <aminasajan2001@gmail.com>
2024-02-29 14:31:53 +0530

    csv read and spark initializer added

---
+++ src/main/scala/com/example/spark/core/CSVReader.scala
@@ -0,0 +1,9 @@
+package com.example.spark.core
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object CSVReader {
+def readcsv(spark:SparkSession,filepath:String):DataFrame={
+  spark.read.option("header","true").option("inferSchema","true").csv(filepath)
+}
+}
